dddd7827142076ef5244b87103150433a17a564d
uthsa <uthsavi29@gmail.com>
2021-01-05 03:44:24 +0530

    Added:To Get Documnets Of Built-in Functions

---
+++ Basic_Python/documents_of_built-in_function.py
@@ -0,0 +1,12 @@
+'''
+* @Author: Uthsavi KP
+* @Date: 2020-01-05 03:42:19 
+* @Last Modified by:   Uthsavi KP
+* @Last Modified time: 2020-01-05 03:45:57
+* @Title : To Get Documnets Of Built-in Functions.
+'''
+
+print(abs.__doc__)
+print(ascii.__doc__)
+print(bin.__doc__)
+print(bool.__doc__)
