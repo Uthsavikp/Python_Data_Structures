cd1d1cbbcce27ad6d480010e5de15adb0ce7b877
uthsa <uthsavi29@gmail.com>
2021-01-05 01:41:43 +0530

    Added:To Sort Three Integers

---
+++ Basic_Python/sort_three_integerts.py
@@ -0,0 +1,11 @@
+'''
+* @Author: Uthsavi KP
+* @Date: 2020-01-05 01:35:09 
+* @Last Modified by:   Uthsavi KP
+* @Last Modified time: 2020-01-05 01:40:37
+* @Title : To Sort Three Integers.
+'''
+
+list_integer = [100,5,55]
+list_integer.sort()
+print(list_integer)
