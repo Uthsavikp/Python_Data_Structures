3b02fb10d16f571ff0b41c2bc9a85353d43b69a7
uthsa <uthsavi29@gmail.com>
2021-01-05 04:07:24 +0530

    Added:To Call An External Command In Python

---
+++ Basic_Python/external_command.py
@@ -0,0 +1,10 @@
+'''
+* @Author: Uthsavi KP
+* @Date: 2020-01-05 04:17:30 
+* @Last Modified by:   Uthsavi KP
+* @Last Modified time: 2020-01-05 04:38:42
+* @Title : To call an external command in Python. 
+'''
+
+from subprocess import call
+call(["ls", "-l"])
