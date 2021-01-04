493db1de8df70b0adf89fd57f9b034b644c05f31
uthsa <uthsavi29@gmail.com>
2021-01-05 03:12:53 +0530

    Added:To Get Users Environment

---
+++ Basic_Python/users_environment.py
@@ -0,0 +1,12 @@
+'''
+* @Author: Uthsavi KP
+* @Date: 2020-01-05 3:12:19 
+* @Last Modified by:   Uthsavi KP
+* @Last Modified time: 2020-01-05 3:20:09
+* @Title : To Get Users Environment.
+'''
+
+import os
+print()
+print(os.environ)
+print()
