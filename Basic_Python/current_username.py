7cfc62e31151412c9b18a85aea7bc85505355505
uthsa <uthsavi29@gmail.com>
2021-01-05 00:45:11 +0530

    Added:To Get The Current User Name

---
+++ Basic_Python/current_username.py
@@ -0,0 +1,11 @@
+'''
+* @Author: Uthsavi KP
+* @Date: 2020-01-04 14:28:56 
+* @Last Modified by:   Uthsavi KP
+* @Last Modified time: 2020-01-04 4:28:56 
+* @Title : Program to get the current username
+
+'''
+
+import getpass
+print(getpass.getuser()) #gets the current user name
