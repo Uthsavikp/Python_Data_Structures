2e2da3799c865069beb455ded39d0cf573c3a5fd
uthsa <uthsavi29@gmail.com>
2021-01-05 04:39:16 +0530

    Added:To Get The System Time

---
+++ Basic_Python/system_time.py
@@ -0,0 +1,12 @@
+'''
+* @Author: Uthsavi KP
+* @Date: 2020-01-05 04:51:24 
+* @Last Modified by:   Uthsavi KP
+* @Last Modified time: 2020-01-05 05:15:14
+* @Title : To Get The System Time.
+'''
+
+import time
+print()
+print(time.ctime())
+print()
