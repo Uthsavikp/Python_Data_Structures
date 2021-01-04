7185e203c03ac27919cb2973d283959338956a54
uthsa <uthsavi29@gmail.com>
2021-01-05 00:50:46 +0530

    Added:To Display The First And Last Colors From The List

---
+++ Basic_Python/colour_display.py
@@ -0,0 +1,13 @@
+'''
+* @Author: Uthsavi KP
+* @Date: 2020-01-04 15:14:56 
+* @Last Modified by:   Uthsavi KP
+* @Last Modified time: 2020-01-04 15:14:56 
+* @Title : To display the first and last colors from the list.
+
+'''
+
+def get_first_last():
+    color_list = ["Red","Green","White" ,"Black"]
+    print(color_list[0:4:3])
+get_first_last()    
