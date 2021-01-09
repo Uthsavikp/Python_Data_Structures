88f9289ef7955ca1950f36e58f665991e0f76b50
uthsa <uthsavi29@gmail.com>
2021-01-09 10:28:22 +0530

    Added:To Add A Key To A Dictionary

---
+++ Dictionary_Programs/add_key.py
@@ -0,0 +1,25 @@
+''' 
+  @Author: Uthsavi KP
+  @Date: 2021-01-07 13:02:55
+  @Last Modified by:  Uthsavi KP
+  @Last Modified time: 2021-01-7 13:34:07
+  @Title: To add a key to a dictionary
+'''
+
+class AddKey:
+    def __init(self):
+        """
+        defining a dictionary
+        """
+        self.dict_elements 
+    def add_key_to_dictionary(self):
+        """
+        adding a new key to the dictionary
+        """
+        self.dict_elements = {0: 10, 1: 20}
+        self.dict_elements["2"] = 30
+        print("new dictionary elements :",self.dict_elements)
+
+if __name__ == "__main__":
+    addkey = AddKey()
+    addkey.add_key_to_dictionary()        
